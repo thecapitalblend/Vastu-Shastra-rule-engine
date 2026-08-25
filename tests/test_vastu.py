617b0f915aea76be3813d2405c020bfb3286d8e1
thecapitalblend <thecapitalblend@gmail.com>
2026-08-25 21:12:47 +0530

    Add files via upload

--- tests/test_vastu.py
+++ tests/test_vastu.py
@@ -1,1 +1,7 @@
+from app.models import Project
+from app.vastu import check_project
 
+def test_validate():
+    p=Project(plot_width_mm=9000,plot_depth_mm=12000)
+    r=check_project(p)
+    assert r['rules_loaded'] >= 1
