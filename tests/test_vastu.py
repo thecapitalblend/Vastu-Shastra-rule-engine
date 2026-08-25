b67db62fbada29da30e2eaf4653cf6def6fae7ba
thecapitalblend <thecapitalblend@gmail.com>
2026-08-25 21:32:56 +0530

    Delete tests directory

--- tests/test_vastu.py
+++
@@ -1,7 +0,0 @@
-from app.models import Project
-from app.vastu import check_project
-
-def test_validate():
-    p=Project(plot_width_mm=9000,plot_depth_mm=12000)
-    r=check_project(p)
-    assert r['rules_loaded'] >= 1
